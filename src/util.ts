6d292577f59864b086bd6b7d3287fd17d1141d99
Paul de Koning <pauldekoning01@gmail.com>
2025-06-08 23:55:06 +0100

    Add user-agent

--- src/util.ts
+++ src/util.ts
@@ -12,7 +12,11 @@ export function getParsedIntOrUndefined(a: string | undefined) {
 export async function getParseHTMLFromURL(url: string) {
     try {
         console.log('Requesting URL', url);
-        const result = await fetch(url);
+        const result = await fetch(url, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36',
+            }
+        });
         const text = await result.text();
         return parse(text);
     } catch (e) {
@@ -23,7 +27,11 @@ export async function getParseHTMLFromURL(url: string) {
 export async function getParseJSONFromURL(url: string) {
     try {
         console.log('Requesting URL', url);
-        const result = await fetch(url);
+        const result = await fetch(url, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36',
+            }
+        });
         return await result.json();
     } catch (e) {
         console.log(`Problem while getting JSON from URL ${url}`, e)
